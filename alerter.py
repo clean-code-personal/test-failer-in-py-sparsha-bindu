acbb8faeb701fa73419afe66a97fb335bb993ceb
sparsha-bindu <85055472+sparsha-bindu@users.noreply.github.com>
2023-02-08 14:38:10 +0530

    Update alerter.py

--- alerter.py
+++ alerter.py
@@ -19,7 +19,4 @@ def alert_in_celcius(farenheit):
         alert_failure_count += 1
     return alert_failure_count
 
-
-alert_in_celcius(400.5)
-alert_in_celcius(303.6)
 print(f'{alert_failure_count} alerts failed.')
