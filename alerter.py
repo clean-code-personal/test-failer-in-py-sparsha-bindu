0b6b9861affa9396bf7ca66e005943b9f45c5c9b
sparsha-bindu <85055472+sparsha-bindu@users.noreply.github.com>
2023-02-08 23:20:19 +0530

    Update alerter.py

--- alerter.py
+++ alerter.py
@@ -16,10 +16,9 @@ def alert_in_celcius(farenheit):
         # However, this code doesn't count failures!
         # Add a test below to catch this bug. Alter the stub above, if needed.
         global alert_failure_count
-        alert_failure_count += 1
-    return alert_failure_count
+        alert_failure_count += 0
+        assert(alert_failure_count > 0)
 
 alert_in_celcius(400.5)
 alert_in_celcius(303.6)
-assert(alert_failure_count > 0)
 print(f'{alert_failure_count} alerts failed.')
