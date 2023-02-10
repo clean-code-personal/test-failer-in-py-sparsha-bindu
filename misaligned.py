c68819eab09023715f1031e224ede3d557be2cf0
sparsha-bindu <85055472+sparsha-bindu@users.noreply.github.com>
2023-02-10 16:28:22 +0530

    Update misaligned.py

--- misaligned.py
+++ misaligned.py
@@ -7,16 +7,3 @@ def print_color_map(): #returns a list of strings, each representing a single ma
         for j, minor in enumerate(minor_colors):
             color_map.append(f'{i * 5 + j} | {major} | {minor}')
     return color_map
-
-def test_print_color_map(): #performs these checks and asserts that the length of the list is 25, which is the number of mappings that should be present
-    color_map = print_color_map()
-    assert len(color_map) == 25
-    for i, color in enumerate(color_map):
-        parts = color.split(" | ")
-        assert int(parts[0]) == i + 1 # Check that the first value starts from 1, not 0
-        assert parts[1] in ["White", "Red", "Black", "Yellow", "Violet"]
-        assert parts[2] in ["Blue", "Orange", "Green", "Brown", "Slate"]
-
-test_print_color_map()
-print("All tests passed!")
-
